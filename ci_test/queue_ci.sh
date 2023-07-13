e50ad3bcd1c5e26a18dd4cdf2c257d8983ec3ddb
170210 <j170210@icloud.com>
2023-07-13 11:40:20 +0900

    add

--- ci_test/queue_ci.sh
+++ ci_test/queue_ci.sh
@@ -12,6 +12,8 @@ executeCheck(){
 }
 
 queryCheck(){
+    echo $1
+    echo $2
     if [[ $1 != $2 ]]; then
         echo "expected result is :$2"
         echo "but query result is:$1"
@@ -26,63 +28,49 @@ init_msg=`jq -nc '{}'`
 INSTANTIATE_RES=`fnsad tx wasm instantiate $CODE_ID $init_msg --label $TOKEN_NAME  --admin $(fnsad keys show $FROM_ACCOUNT -a --keyring-backend test) --from $FROM_ACCOUNT --keyring-backend test --chain-id finschia -b block -o json -y`
 CONTRACT_ADDRESS=`echo $INSTANTIATE_RES | jq '.logs[] | select(.msg_index == 0) | .events[] | select(.type == "instantiate") | .attributes[] | select(.key == "_contract_address") | .value' | sed 's/"//g'`
 
-# enqueue in order
+# check enqueue
 # now: {100, 200, 300}
 for value in 100 200 300; do
     enqueue_msg=`jq -nc --arg value $value '{enqueue:{value:($value | tonumber)}}'`
-    RUN_INFO=$(fnsad tx wasm execute $CONTRACT_ADDRESS $enqueue_msg --from $FROM_ACCOUNT --keyring-backend test --chain-id finschia -b block -y)
-    executeCheck $RUN_INFO "enqueue_error"
+    run_info=$(fnsad tx wasm execute $CONTRACT_ADDRESS $enqueue_msg --from $FROM_ACCOUNT --keyring-backend test --chain-id finschia -b block -y)
+    executeCheck $run_info "enqueue_error"
 done
 
-# the result should be 3
+# check count
 expected_result='data: count: 3'
-
 count_msg=`jq -nc '{count:{}}'`
 query_result=$(fnsad query wasm contract-state smart $CONTRACT_ADDRESS $count_msg)
 executeCheck $query_result "query_error"
 queryCheck $query_result $expected_result
 
-# dequeue
+# check dequeue
 # now: {200, 300}
 dequeue_msg=`jq -nc '{dequeue:{}}'`      
-RUN_INFO=$(fnsad tx wasm execute $CONTRACT_ADDRESS $dequeue_msg --from $FROM_ACCOUNT --keyring-backend test --chain-id finschia -b block -y)
-executeCheck $RUN_INFO "enqueue_error"
+run_info=$(fnsad tx wasm execute $CONTRACT_ADDRESS $dequeue_msg --from $FROM_ACCOUNT --keyring-backend test --chain-id finschia -b block -y)
+executeCheck $run_info "enqueue_error"
 
-# the result should be 500
+# check sum
 expected_result='data: sum: 500'
-
 sum_msg=`jq -nc '{sum:{}}'`
 query_result=$(fnsad query wasm contract-state smart $CONTRACT_ADDRESS $sum_msg)
 executeCheck $query_result "query_error"
 queryCheck $query_result $expected_result
 
-# the result should be 
-# counters:
-# - - 200
-#   - 300
-# - - 300
-#   - 0
+# check reducer
 expected_result='data: counters: - - 200 - 300 - - 300 - 0'
-
 reducer_msg=`jq -nc '{reducer:{}}'`
 query_result=$(fnsad query wasm contract-state smart $CONTRACT_ADDRESS $reducer_msg)
 executeCheck $query_result "query_error"
 queryCheck $query_result $expected_result
 
-# the result should be
-# early:
-# - 1
-# - 2
-# empty: []
-# late: []
+# check list
 expected_result='data: early: - 1 - 2 empty: [] late: []'
-
 list_msg=`jq -nc '{list:{}}'`
 query_result=$(fnsad query wasm contract-state smart $CONTRACT_ADDRESS $list_msg)
 executeCheck $query_result "query_error"
 queryCheck $query_result $expected_result
 
-#
+# check open_iterators
 openIterators_msg=`jq -nc '{open_iterators:{count:3}}'`
 query_result=$(fnsad query wasm contract-state smart $CONTRACT_ADDRESS $openIterators_msg)
 executeCheck $query_result "query_error"
